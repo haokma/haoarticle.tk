5936b107f54a56e7c00ba3f3802b2c786f05a228
hao2001vt <nguyenchihao2001vt@gmail.com>
2021-11-09 10:58:56 +0700

    add footer

--- components/common/footer/index.tsx
+++ components/common/footer/index.tsx
@@ -1,7 +1,35 @@
 import * as React from 'react';
 import styles from './footer.module.scss';
+import Link from 'next/link';
 export interface FooterProps {}
 
 export default function Footer(props: FooterProps) {
-  return <div className={styles.footer}></div>;
+  return (
+    <div className={styles.footer}>
+      <div className="container">
+        <div className={styles.footerWrapper}>
+          <div className={styles.footerTitle}>
+            <p>© 2021 - Microvn's Blog</p>
+          </div>
+          <ul className={styles.navList}>
+            <li className={styles.navItem}>
+              <Link href="">
+                <a className={styles.navLink}>LinkedIn</a>
+              </Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="">
+                <a className={styles.navLink}>Facebook</a>
+              </Link>
+            </li>
+            <li className={styles.navItem}>
+              <Link href="">
+                <a className={styles.navLink}>Github</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
