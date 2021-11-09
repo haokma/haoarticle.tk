14f03b2043954eb294ea4d4c9879aa5dc10ca17f
hao2001vt <nguyenchihao2001vt@gmail.com>
2021-11-09 10:45:58 +0700

    add footer

--- components/common/footer/index.tsx
+++ components/common/footer/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-
+import styles from './footer.module.scss';
 export interface FooterProps {}
 
 export default function Footer(props: FooterProps) {
-  return <div></div>;
+  return <div className={styles.footer}></div>;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from '@/components/common';
+import Footer from '@/components/common/footer';
 import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <div className="main">
       <Header />
+      <Footer />
     </div>
   );
 };
