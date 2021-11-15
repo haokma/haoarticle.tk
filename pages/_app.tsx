32e372500324973641b8f33d7293e46dd580a5db
hao2001vt <nguyenchihao2001vt@gmail.com>
2021-11-15 18:09:04 +0700

    analy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,9 @@ import { Layout } from '@/components/common/layouts';
 import { SWRConfig } from 'swr';
 import '../styles/globals.scss';
 import '../styles/Home.module.scss';
-
+import Head from 'next/head';
+import Script from 'next/script';
+import * as gtag from '@/lib/gtag';
 function MyApp({ Component, pageProps }: any) {
   const LayoutDefault = Component.Layout || Layout;
   return (
@@ -13,6 +15,26 @@ function MyApp({ Component, pageProps }: any) {
       }}
     >
       <LayoutDefault>
+        <Head>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
         <Component {...pageProps} />
       </LayoutDefault>
     </SWRConfig>
